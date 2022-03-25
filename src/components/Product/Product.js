f679df4ec8f91995ffc3c34227248cb812cefc25
pushpitanandy <nandypushpita@gmail.com>
2022-03-25 16:34:57 +0600

    product card edited and btn added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,9 +6,15 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <div>
+            <div className='product-info'>
                 <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
             </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
