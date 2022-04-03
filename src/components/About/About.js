93a9fbe87e0fcf2a6be96dbce69188bac411610b
pushpitanandy <nandypushpita@gmail.com>
2022-04-03 18:50:23 +0600

    loading data from custom hook

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>We are secret.</h2>
+            <p>But you can know about us.</p>
+        </div>
+    );
+};
+
+export default About;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,16 +3,12 @@ import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
+import useProducts from '../../hooks/useProducts';
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useProducts();
     const [cart, setCart] = useState([]);
 
-    useEffect(() => {
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data => setProducts(data));
-    }, []);
 
     useEffect(() => {
         const storedCart = getStoredCart();
