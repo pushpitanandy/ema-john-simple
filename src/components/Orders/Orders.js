b6e09aba40b0f0bf77aa2adb4d3a09f87281d49e
pushpitanandy <nandypushpita@gmail.com>
2022-04-03 19:16:46 +0600

    loading cart by custom hook

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import useProducts from '../../hooks/useProducts';
+import useCart from '../../hooks/useCart';
+
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
         <div>
             <h2>This is Orders {products.length}</h2>
+            <p>Cart has: {cart.length}</p>
         </div>
     );
 };
